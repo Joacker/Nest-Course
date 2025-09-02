b45215f85e2edade6435ef4437570706df45c1d6
Joacker <jfernandezvizcarra@gmail.com>
2025-09-02 10:53:48 -0400

    adding new features for nest course

--- myfirstApp/src/tasks/tasks.controller.ts
+++ myfirstApp/src/tasks/tasks.controller.ts
@@ -1,13 +1,19 @@
 import { Controller, Get } from '@nestjs/common'
+import { TasksService } from './tasks.service';
 
 @Controller({})
 export class TaskController {
+    tasksService:TasksService;
+
+    constructor(tasksService: TasksService) {
+        this.tasksService = tasksService;
+    }
 
     @Get('/tasks')
     getAllTasks() {
         // buscar en una bd
         // peticion a otro backen o api
-        return 'Obteniendo todas las tareas';
+        return this.tasksService.getTasks();
     }
 
     // @Get('/')
---
+++ myfirstApp/src/users/users.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UsersService {
+
+    private users = [
+        {
+            id: 1,
+            name: 'John Doe',
+            phone: '1234567890'
+        }
+    ]
+
+    getUsers() {
+        return this.users;
+    }
+
+}
